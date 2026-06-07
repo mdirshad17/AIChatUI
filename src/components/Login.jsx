5fa0151601f16fb0e428bc233a7542ed75d16290
mdirshad <irshadvmc@gmail.com>
2026-06-07 12:06:04 +0530

    changes for login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,13 +2,12 @@
 import React from "react";
 export default function Login() {
   const handleGoogleLogin = () => {
-    // Redirect directly to the Spring Boot backend trigger URL
-    window.location.href = 'http://3.108.194.133:8080/oauth2/authorization/google';
-  };
+  window.location.href = 'http://3.108.194.133.nip.io:8080/oauth2/authorization/google';
+};
 
-  const handleGithubLogin = () => {
-    window.location.href = 'http://3.108.194.133:8080/oauth2/authorization/github';
-  };
+const handleGithubLogin = () => {
+  window.location.href = 'http://3.108.194.133.nip.io:8080/oauth2/authorization/github';
+};
 
   return (
   
